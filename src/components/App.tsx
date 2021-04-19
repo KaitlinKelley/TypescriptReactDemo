dec5e4306b22537094d5f48f3f9887d0754f0a09
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-04-19 18:32:41 -0500

    Finished typescript course

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,10 +10,14 @@ type InvoiceDataType = {
   invoices: InvoiceType[];
 }
 
+// "|" means unions
+type PaymentStatusType = "paid" | "pending" | "late";
+
 type InvoiceType = {
-  id:number, 
-  name:string, 
-  total:string
+  id:number;
+  name:string; 
+  total:string;
+  paymentStatus:PaymentStatusType;
 };
 
 //props means properties
@@ -32,7 +36,7 @@ const InvoiceList = (props: IInvoiceListProps) =>{
           //key prop must be unique!!!
           <div key={invoice.id} style={itemStyleContainer}>
               <div>{invoice.name}</div>
-              <div>{invoice.total}</div>
+              <div>{invoice.total} - {invoice.paymentStatus}</div>
           </div>
           ))}
       </div>
@@ -45,9 +49,9 @@ const App = () => {
   const data = {
     customerName: "Google",
     invoices: [
-      {id:123, name:"Dev work", total: "20.00"}, 
-      {id:456, name:"More dev work", total: "50.00"},
-      {id:789, name:"AGAIN MORE dev work", total: "100.00"}
+      {id:123, name:"Dev work", total: "20.00", paymentStatus: "paid"}, 
+      {id:456, name:"More dev work", total: "50.00", paymentStatus: "pending"},
+      {id:789, name:"AGAIN MORE dev work", total: "100.00", paymentStatus: "paid"}
     ]
   }
   return (
