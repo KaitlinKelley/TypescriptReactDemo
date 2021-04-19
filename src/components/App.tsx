508ab357fe2491fa2d49b349f074246d84148fcf
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2021-04-19 18:22:53 -0500

    Almost done with Typescript course

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,58 @@
 import * as React from "react";
 
-import Hello from "./Hello";
-import HelloWithHooks from "./HelloWithHooks";
+interface IInvoiceListProps {
+  invoiceData: InvoiceDataType;
+  logo?:string;
+}
 
-const App = () => {
+type InvoiceDataType = {
+  customerName:string;
+  invoices: InvoiceType[];
+}
+
+type InvoiceType = {
+  id:number, 
+  name:string, 
+  total:string
+};
+
+//props means properties
+const InvoiceList = (props: IInvoiceListProps) =>{
+  const {customerName, invoices} = props.invoiceData;
+
+  const itemStyleContainer = {display: "flex", justifyContent: "space-between"};
+
   return (
     <div>
-      <Hello compiler={"TS"} framework={"React"} />
-      <HelloWithHooks />
+      <h1>{customerName}</h1>
+      <hr/>
+
+      <div>
+        {invoices.map((invoice) => (
+          //key prop must be unique!!!
+          <div key={invoice.id} style={itemStyleContainer}>
+              <div>{invoice.name}</div>
+              <div>{invoice.total}</div>
+          </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+
+const App = () => {
+  const data = {
+    customerName: "Google",
+    invoices: [
+      {id:123, name:"Dev work", total: "20.00"}, 
+      {id:456, name:"More dev work", total: "50.00"},
+      {id:789, name:"AGAIN MORE dev work", total: "100.00"}
+    ]
+  }
+  return (
+    <div>
+      <InvoiceList invoiceData={data} />
     </div>
   );
 };
